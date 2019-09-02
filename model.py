460d2a64814d2c57c05c0427c25ea512ada7fb41
Rajat Roy <34815254+Rajat-Roy@users.noreply.github.com>
2019-09-02 13:49:48 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# import keras, csv, cv2 and numpy library
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Conv2D
@@ -8,10 +9,12 @@ import cv2
 import numpy as np
 
 def Preprocess(datacsv):
-    lines = []
-    images = []
-    measurements = []
+    lines = [] # rows in csv log file
+    images = [] # images in data folder recorded from simulator
+    measurements = [] # steering angles from csv file
 
+    # read the csv file and append the three camera angle 
+    # images to images list and correct the steering angles
     with open(datacsv) as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
@@ -28,7 +31,7 @@ def Preprocess(datacsv):
                     measurement -= 0.2
                     
                 measurements.append(measurement)
-         
+    # augment images and steering angles by mirroring them     
     augmented_images, augmented_measurements = [], []
     for image, measurement in zip(images, measurements):
         augmented_images.append(image)
@@ -36,7 +39,10 @@ def Preprocess(datacsv):
         augmented_images.append(cv2.flip(image, 1))
         augmented_measurements.append(measurement*-1.0)      
     return np.array(augmented_images), np.array(augmented_measurements)
-   
+
+# train using Nvidia model architecture and save the model
+# Note: This fails to save model in windows machine,
+# but in ipython notebook it is succesful
 def Save_Model(X_train, y_train, epoch=2):
     model = Sequential()
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
@@ -49,16 +55,14 @@ def Save_Model(X_train, y_train, epoch=2):
     model.add(Conv2D(64,(3,3), activation='relu'))
     model.add(Flatten())
     model.add(Dense(100))
-    model.add(Dropout(rate=drop_rate))
     model.add(Dense(50))
-    model.add(Dropout(rate=drop_rate))
     model.add(Dense(10))
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
     training = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=epoch)
     model.save('model.h5')
-
+# single function for csv input and saved model output 
 def Execute(epoch=2):
     X_train, y_train = Preprocess('./data/driving_log.csv')
     Save_Model(X_train, y_train, epoch)
